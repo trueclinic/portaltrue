81b1a36bd5edcd4314baeb37c02647d275346db9
Ricardo Wilbert Oliveira <ricardowilbertoliveira@MacBook-Pro-de-Ricardo.local>
2025-09-01 12:50:36 +0100

    feat(auth): fluxo de esqueci a palavra‑passe (views padrão Django + templates)

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 
@@ -27,6 +29,39 @@ urlpatterns = [
     path('', include('consulta.urls')),
     path('', include('autenticador.urls')),
     path('', include('ticket.urls')),
+    # Reset de palavra‑passe (e-mail)
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            email_template_name='registration/password_reset_email.html',
+            subject_template_name='registration/password_reset_subject.txt',
+            success_url=reverse_lazy('password_reset_done'),
+        ),
+        name='password_reset',
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html',
+            success_url=reverse_lazy('password_reset_complete'),
+        ),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete',
+    ),
     
 ]
 
